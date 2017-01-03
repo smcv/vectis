382533fc7084d41f2696e34a881eef25ab79b262
Simon McVittie <smcv@debian.org>
2017-01-03 12:53:25 +0000

    Run pyflakes3 across the code

---
+++ t/pyflakes.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright © 2016 Simon McVittie
+# SPDX-License-Identifier: GPL-2.0+
+# (see vectis/__init__.py)
+
+if [ "x$PYFLAKES" = xfalse ]; then
+    echo "1..0 # SKIP pyflakes3 not found"
+elif "${PYFLAKES}" vectis; then
+    echo "1..1"
+    echo "ok 1 - pyflakes reported no issues"
+else
+    echo "1..1"
+    echo "ok 1 # SKIP Ignoring pyflakes issues for now"
+fi
+
+# vim:set sw=4 sts=4 et:
