b585080e8de881e31f38e53323a6d0a54242dccd
Simon McVittie <smcv@debian.org>
2017-02-05 15:49:35 +0000

    sbuild: Explicitly pass -j1 if parallelization is not desired

--- vectis/commands/sbuild.py
+++ vectis/commands/sbuild.py
@@ -24,13 +24,14 @@ def get_dpkg_buildpackage_options(args, suite):
     if args._versions_since:
         argv.append('-v{}'.format(args._versions_since))
 
-    if args.sbuild_force_parallel > 1:
+    if args.sbuild_force_parallel:
         argv.append('-j{}'.format(args.sbuild_force_parallel))
-    elif args.parallel != 1:
-        if args.parallel:
-            argv.append('-J{}'.format(args.parallel))
-        else:
-            argv.append('-Jauto')
+    elif args.parallel == 1:
+        argv.append('-j1')
+    elif args.parallel:
+        argv.append('-J{}'.format(args.parallel))
+    else:
+        argv.append('-Jauto')
 
     for a in get_dpkg_source_options(args):
         argv.append('--source-option=' + a)
