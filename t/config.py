2b61a56cda8c19174c458ab2f20d609adf1de952
Simon McVittie <smcv@debian.org>
2017-02-09 22:25:52 +0000

    t/config: Improve test coverage

--- t/config.py
+++ t/config.py
@@ -13,6 +13,14 @@ from vectis.config import (
         ConfigError,
         )
 
+XDG_CACHE_HOME = os.getenv('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
+
+try:
+    ARCHITECTURE = subprocess.check_call(['dpkg', '--print-architecture'],
+            universal_newlines=True).strip()
+except:
+    ARCHITECTURE = None
+
 class DefaultsTestCase(unittest.TestCase):
     def setUp(self):
         self.__config = Config(config_layers=(dict(
@@ -25,6 +33,7 @@ class DefaultsTestCase(unittest.TestCase):
         self.__config = Config(config_layers=({},), current_directory='/')
 
         self.assertGreaterEqual(self.__config.parallel, 1)
+        self.assertIs(type(self.__config.parallel), int)
 
         debian = self.__config._get_vendor('debian')
         ubuntu = self.__config._get_vendor('ubuntu')
@@ -34,17 +43,50 @@ class DefaultsTestCase(unittest.TestCase):
         self.assertIs(self.__config.vendor, debian)
         self.assertIs(self.__config.worker_vendor, debian)
         self.assertEqual(self.__config.archive, 'debian')
+        self.assertEqual(self.__config.apt_cacher_ng, None)
         self.assertEqual(self.__config.force_parallel, 0)
         self.assertIs(self.__config.sbuild_together, False)
         self.assertEqual(self.__config.output_builds, '..')
+        self.assertEqual(self.__config.size, '42G')
         self.assertIsNone(self.__config.sbuild_buildables)
+        self.assertEqual(self.__config.sbuild_resolver, [])
+        self.assertEqual(self.__config.debootstrap_script, 'sid')
+        self.assertIsNone(self.__config.apt_key)
+        self.assertIsNone(self.__config.apt_suite)
+        self.assertEqual(self.__config.dpkg_source_tar_ignore, [])
+        self.assertIsNone(self.__config.dpkg_source_diff_ignore)
+        self.assertEqual(self.__config.dpkg_source_extend_diff_ignore, [])
 
-        # FIXME: these contain the suite, which is undefined, so their values
-        # are not useful - for now just assert they don't crash
-        self.assertIsNotNone(self.__config.debootstrap_script)
-        self.assertIsNotNone(self.__config.qemu_image)
-        self.__config.suite
+        if ARCHITECTURE is not None:
+            self.assertEqual(self.__config.architecture, ARCHITECTURE)
+            self.assertEqual(self.__config.qemu_image,
+                    '{}/vectis/vectis-debian-sid-{}.qcow2'.format(
+                        XDG_CACHE_HOME, ARCHITECTURE))
+            self.assertEqual(self.__config.autopkgtest_qemu_image,
+                    '{}/vectis/vectis-debian-sid-{}.qcow2'.format(
+                        XDG_CACHE_HOME, ARCHITECTURE))
+            self.assertEqual(self.__config.write_qemu_image,
+                    '{}/vectis/vectis-debian-sid-{}.qcow2'.format(
+                        XDG_CACHE_HOME, ARCHITECTURE))
+            self.assertEqual(self.__config.worker_architecture, ARCHITECTURE)
+            self.assertEqual(self.__config.worker,
+                    'qemu {}'.format(self.__config.worker_qemu_image))
+            self.assertEqual(self.__config.worker_qemu_image,
+                    '{}/vectis/vectis-debian-sid-{}.qcow2'.format(
+                        XDG_CACHE_HOME, ARCHITECTURE))
+            self.assertEqual(self.__config.sbuild_worker,
+                    'qemu {}'.format(self.__config.sbuild_worker_qemu_image))
+            self.assertEqual(self.__config.sbuild_worker_qemu_image,
+                    '{}/vectis/vectis-debian-sid-{}.qcow2'.format(
+                        XDG_CACHE_HOME, ARCHITECTURE))
 
+        sid = self.__config.vendor.get_suite('sid')
+        self.assertIs(self.__config.autopkgtest, True)
+        self.assertEqual(self.__config.suite, sid)
+        self.assertEqual(self.__config.worker_suite, sid)
+        jb = self.__config.vendor.get_suite('jessie-apt.buildd.debian.org')
+        # FIXME: should be a Suite?
+        self.assertEqual(self.__config.sbuild_worker_suite, str(jb))
         self.assertEqual(self.__config.default_suite, 'sid')
         self.assertEqual(self.__config.components, {'main'})
         self.assertEqual(self.__config.extra_components,
@@ -53,10 +95,7 @@ class DefaultsTestCase(unittest.TestCase):
             'contrib', 'non-free'})
 
         self.assertEqual(self.__config.storage,
-            '{}/vectis'.format(os.getenv('XDG_CACHE_HOME',
-                os.path.expanduser('~/.cache'))))
-        self.assertEqual(self.__config.worker,
-            'qemu {}'.format(self.__config.worker_qemu_image))
+            '{}/vectis'.format(XDG_CACHE_HOME))
 
         with self.assertRaises(ConfigError):
             self.__config.mirror
@@ -108,11 +147,16 @@ class DefaultsTestCase(unittest.TestCase):
         self.assertEqual(debian.vendor, debian)
         self.assertEqual(debian.worker_vendor, 'debian')
         self.assertEqual(debian.sbuild_worker_suite, 'jessie-apt.buildd.debian.org')
-        #self.assertEqual(debian.archive, 'debian')
-        #self.assertEqual(debian.mirror, 'http://192.168.122.1:3142/debian')
+        self.assertEqual(debian.archive, 'debian')
+        self.assertEqual(debian.mirror, 'http://192.168.122.1:3142/debian')
         self.assertIsNone(debian.get_suite('xenial', create=False))
         self.assertEqual(debian.get_suite('experimental').sbuild_resolver[0],
                 '--build-dep-resolver=aspcud')
+
+        potato = debian.get_suite('potato', True)
+        sec = debian.get_suite('potato-security', True)
+        self.assertEqual(list(potato.hierarchy), [potato])
+        self.assertEqual(list(sec.hierarchy), [sec, potato])
 
         self.assertEqual(str(ubuntu), 'ubuntu')
         self.assertEqual(ubuntu.components, {'main'})
@@ -122,8 +166,8 @@ class DefaultsTestCase(unittest.TestCase):
             'restricted', 'multiverse'})
         self.assertEqual(ubuntu.vendor, ubuntu)
         self.assertEqual(ubuntu.worker_vendor, 'ubuntu')
-        #self.assertEqual(ubuntu.archive, 'ubuntu')
-        #self.assertEqual(ubuntu.mirror, 'http://192.168.122.1:3142/ubuntu')
+        self.assertEqual(ubuntu.archive, 'ubuntu')
+        self.assertEqual(ubuntu.mirror, 'http://192.168.122.1:3142/ubuntu')
         self.assertIsNone(ubuntu.get_suite('unstable', create=False))
         self.assertIsNone(ubuntu.get_suite('stable', create=False))
 
@@ -134,10 +178,8 @@ class DefaultsTestCase(unittest.TestCase):
         self.assertEqual(steamos.components, {'main'})
         self.assertEqual(steamos.vendor, steamos)
         self.assertEqual(steamos.worker_vendor, 'debian')
-        # FIXME: fails: is "${vendor}"
-        #self.assertEqual(steamos.archive, 'steamos')
-        # FIXME: fails: ends with "${archive}"
-        #self.assertEqual(steamos.mirror, 'http://192.168.122.1:3142/steamos')
+        self.assertEqual(steamos.archive, 'steamos')
+        self.assertEqual(steamos.mirror, 'http://192.168.122.1:3142/steamos')
 
         self.assertIsNone(steamos.get_suite('xyzzy', create=False))
         self.assertIsNotNone(steamos.get_suite('xyzzy'))
@@ -161,49 +203,49 @@ class DefaultsTestCase(unittest.TestCase):
         try:
             import distro_info
         except ImportError:
-            pass
-        else:
-            debian_info = distro_info.DebianDistroInfo()
-            ubuntu_info = distro_info.UbuntuDistroInfo()
+            return
 
-            try:
-                ubuntu_devel = ubuntu_info.devel()
-            except distro_info.DistroDataOutdated:
-                ubuntu_devel = ubuntu_info.stable()
+        debian_info = distro_info.DebianDistroInfo()
+        ubuntu_info = distro_info.UbuntuDistroInfo()
 
-            self.assertEqual(str(ubuntu.get_suite('devel')), ubuntu_devel)
-            self.assertEqual(str(debian.get_suite('unstable')),
-                    'sid')
-            self.assertEqual(str(debian.get_suite('testing')),
-                    debian_info.testing())
-            self.assertEqual(str(debian.get_suite('oldstable')),
-                    debian_info.old())
-            self.assertEqual(str(debian.get_suite('rc-buggy')),
-                    'experimental')
+        try:
+            ubuntu_devel = ubuntu_info.devel()
+        except distro_info.DistroDataOutdated:
+            ubuntu_devel = ubuntu_info.stable()
 
-            stable = debian.get_suite('stable')
-            self.assertEqual(str(stable), debian_info.stable())
-            self.assertEqual(stable.sbuild_resolver, [])
+        self.assertEqual(str(ubuntu.get_suite('devel')), ubuntu_devel)
+        self.assertEqual(str(debian.get_suite('unstable')),
+                'sid')
+        self.assertEqual(str(debian.get_suite('testing')),
+                debian_info.testing())
+        self.assertEqual(str(debian.get_suite('oldstable')),
+                debian_info.old())
+        self.assertEqual(str(debian.get_suite('rc-buggy')),
+                'experimental')
 
-            backports = debian.get_suite('stable-backports')
-            self.assertEqual(str(backports),
-                    debian_info.stable() + '-backports')
-            self.assertEqual(backports.sbuild_resolver,
-                    ['--build-dep-resolver=aptitude'])
-            self.assertEqual(backports.apt_suite,
-                    debian_info.stable() + '-backports')
-            self.assertEqual(backports.mirror,
-                    'http://192.168.122.1:3142/debian')
-            self.assertEqual(backports.hierarchy[0], backports)
-            self.assertEqual(str(backports.hierarchy[1]), str(stable))
+        stable = debian.get_suite('stable')
+        self.assertEqual(str(stable), debian_info.stable())
+        self.assertEqual(stable.sbuild_resolver, [])
 
-            security = debian.get_suite('stable-security')
-            self.assertEqual(security.apt_suite,
-                    '{}/updates'.format(debian_info.stable()))
-            self.assertEqual(security.mirror,
-                    'http://192.168.122.1:3142/security.debian.org')
-            self.assertEqual(security.hierarchy[0], security)
-            self.assertEqual(str(security.hierarchy[1]), str(stable))
+        backports = debian.get_suite('stable-backports')
+        self.assertEqual(str(backports),
+                debian_info.stable() + '-backports')
+        self.assertEqual(backports.sbuild_resolver,
+                ['--build-dep-resolver=aptitude'])
+        self.assertEqual(backports.apt_suite,
+                debian_info.stable() + '-backports')
+        self.assertEqual(backports.mirror,
+                'http://192.168.122.1:3142/debian')
+        self.assertEqual(backports.hierarchy[0], backports)
+        self.assertEqual(str(backports.hierarchy[1]), str(stable))
+
+        security = debian.get_suite('stable-security')
+        self.assertEqual(security.apt_suite,
+                '{}/updates'.format(debian_info.stable()))
+        self.assertEqual(security.mirror,
+                'http://192.168.122.1:3142/security.debian.org')
+        self.assertEqual(security.hierarchy[0], security)
+        self.assertEqual(str(security.hierarchy[1]), str(stable))
 
     def tearDown(self):
         pass
