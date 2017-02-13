ee020ec18ab2d24aa0bf20239f0d53e97fcf9182
Simon McVittie <smcv@debian.org>
2017-02-13 16:28:47 +0000

    Add initial Travis-CI integration

---
+++ t/ci-build.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Copyright © 2015-2016 Collabora Ltd.
+# Copyright © 2017 Simon McVittie
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -euo pipefail
+set -x
+
+NULL=
+
+# ci_docker:
+# If non-empty, this is the name of a Docker image. ci-install.sh will
+# fetch it with "docker pull" and use it as a base for a new Docker image
+# named "ci-image" in which we will do our testing.
+#
+# If empty, we test on "bare metal".
+# Typical values: ubuntu:xenial, debian:jessie-slim
+: "${ci_docker:=}"
+
+# ci_host:
+# See ci-install.sh
+: "${ci_host:=native}"
+
+# ci_parallel:
+# A number of parallel jobs, passed to make -j
+: "${ci_parallel:=1}"
+
+# ci_sudo:
+# If yes, assume we can get root using sudo; if no, only use current user
+: "${ci_sudo:=no}"
+
+# ci_test:
+# If yes, run tests; if no, just build
+: "${ci_test:=yes}"
+
+# ci_test_fatal:
+# If yes, test failures break the build; if no, they are reported but ignored
+: "${ci_test_fatal:=yes}"
+
+if [ -n "$ci_docker" ]; then
+    exec docker run \
+        --env=ci_docker="" \
+        --env=ci_host="${ci_host}" \
+        --env=ci_parallel="${ci_parallel}" \
+        --env=ci_sudo=yes \
+        --env=ci_test="${ci_test}" \
+        --env=ci_test_fatal="${ci_test_fatal}" \
+        --privileged \
+        ci-image \
+        tools/ci-build.sh
+fi
+
+maybe_fail_tests () {
+    if [ "$ci_test_fatal" = yes ]; then
+        exit 1
+    fi
+}
+
+NOCONFIGURE=1 ./autogen.sh
+
+srcdir="$(pwd)"
+mkdir ci-build
+cd ci-build
+
+make="make -j${ci_parallel} V=1 VERBOSE=1"
+
+../configure \
+    --enable-installed-tests \
+    --enable-maintainer-mode \
+    --enable-modular-tests \
+    --with-glib \
+    ${NULL}
+
+${make}
+[ "$ci_test" = no ] || ${make} check || maybe_fail_tests
+cat test/test-suite.log || :
+[ "$ci_test" = no ] || ${make} distcheck || maybe_fail_tests
+
+${make} install DESTDIR=$(pwd)/DESTDIR
+( cd DESTDIR && find . )
+
+if [ "$ci_sudo" = yes ] && [ "$ci_test" = yes ]; then
+    export VECTIS_TEST_SUDO=sudo
+    export VECTIS_TEST_DEBIAN_MIRROR=http://deb.debian.org/debian
+    export VECTIS_TEST_UBUNTU_MIRROR=http://archive.debian.com/ubuntu
+
+    env LD_LIBRARY_PATH=/usr/local/lib \
+    gnome-desktop-testing-runner -d /usr/local/share vectis || \
+        maybe_fail_tests
+fi
+
+# vim:set sw=4 sts=4 et:
---
+++ t/ci-install.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Copyright © 2015-2016 Collabora Ltd.
+# Copyright © 2017 Simon McVittie
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -euo pipefail
+set -x
+
+NULL=
+
+# ci_distro:
+# OS distribution in which we are testing
+# Typical values: ubuntu, debian; maybe fedora in future
+: "${ci_distro:=ubuntu}"
+
+# ci_docker:
+# If non-empty, this is the name of a Docker image. ci-install.sh will
+# fetch it with "docker pull" and use it as a base for a new Docker image
+# named "ci-image" in which we will do our testing.
+: "${ci_docker:=}"
+
+# ci_host:
+# Either "native", or an Autoconf --host argument to cross-compile
+# the package
+: "${ci_host:=native}"
+
+# ci_in_docker:
+# Used internally by ci-install.sh. If yes, we are inside the Docker image
+# (ci_docker is empty in this case).
+: "${ci_in_docker:=no}"
+
+# ci_suite:
+# OS suite (release, branch) in which we are testing.
+# Typical values for ci_distro=debian: sid, jessie
+# Typical values for ci_distro=fedora might be 25, rawhide
+: "${ci_suite:=trusty}"
+
+if [ $(id -u) = 0 ]; then
+    sudo=
+else
+    sudo=sudo
+fi
+
+if [ -n "$ci_docker" ]; then
+    sed \
+        -e "s/@ci_distro@/${ci_distro}/" \
+        -e "s/@ci_docker@/${ci_docker}/" \
+        -e "s/@ci_suite@/${ci_suite}/" \
+        < tools/ci-Dockerfile.in > Dockerfile
+    exec docker build -t ci-image .
+fi
+
+case "$ci_distro" in
+    (debian|ubuntu)
+        # Don't ask questions, just do it
+        sudo="$sudo env DEBIAN_FRONTEND=noninteractive"
+
+        # Debian Docker images use httpredir.debian.org but it seems to be
+        # unreliable; use a CDN instead
+        $sudo sed -i -e 's/httpredir\.debian\.org/deb.debian.org/g' \
+            /etc/apt/sources.list
+
+        $sudo apt-get -qq -y --no-install-recommends install \
+            autoconf \
+            autoconf-archive \
+            automake \
+            autopkgtest \
+            autotools-dev \
+            build-essential \
+            debhelper \
+            devscripts \
+            gnome-desktop-testing \
+            python3-dev \
+            python3-distro-info \
+            python3-tap \
+            qemu-system \
+            qemu-utils \
+            vmdebootstrap \
+            ${NULL}
+
+        if [ "$ci_in_docker" = yes ]; then
+            # Add the user that we will use to do the build inside the
+            # Docker container, and let them use sudo
+            adduser --disabled-password user </dev/null
+            apt-get -y install sudo
+            echo "user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nopasswd
+            chmod 0440 /etc/sudoers.d/nopasswd
+        fi
+
+        case "$ci_suite" in
+            (jessie|trusty|xenial)
+                # Avoid ye olde autoconf-archive
+                wget http://snapshot.debian.org/archive/debian/20161011T051357Z/pool/main/a/autoconf-archive/autoconf-archive_20160916-1_all.deb
+                $sudo dpkg -i autoconf-archive_*_all.deb
+                rm autoconf-archive_*_all.deb
+                ;;
+        esac
+        ;;
+
+    (*)
+        echo "Don't know how to set up ${ci_distro}" >&2
+        exit 1
+        ;;
+esac
+
+# vim:set sw=4 sts=4 et:
