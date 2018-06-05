4023184fd7b3d3dca6010332f715da20e6d55371
Simon McVittie <smcv@debian.org>
2018-06-05 09:01:39 +0100

    Improve pyflakes test script
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- t/pyflakes.sh
+++ t/pyflakes.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-# Copyright © 2016 Simon McVittie
+# Copyright © 2016-2018 Simon McVittie
 # SPDX-License-Identifier: GPL-2.0+
 # (see vectis/__init__.py)
 
-if [ "x${PYFLAKES:=pyflakes3}" = xfalse ]; then
+if [ "x${PYFLAKES:=pyflakes3}" = xfalse ] || \
+        [ -z "$(command -v "$PYFLAKES")" ]; then
     echo "1..0 # SKIP pyflakes3 not found"
-elif "${PYFLAKES}" vectis; then
+elif "${PYFLAKES}" vectis >&2; then
     echo "1..1"
-    echo "ok 1 - pyflakes reported no issues"
+    echo "ok 1 - $PYFLAKES reported no issues"
 else
     echo "1..1"
-    echo "not ok 1 # TODO pyflakes issues reported"
+    echo "not ok 1 # TODO $PYFLAKES issues reported"
 fi
 
 # vim:set sw=4 sts=4 et:
